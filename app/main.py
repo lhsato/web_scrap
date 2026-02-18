7f27b7ae4c0a4d88116eccb774629ccc49d8aa66
lhsato.kh <lhsato.kh@gmail.com>
2026-02-18 21:03:15 +0700

    4. Python Basics for Web Scraping 2:20

---
+++ app/main.py
@@ -0,0 +1,4 @@
+provices = ["Banteay Meanchey", "Battambang", "Kampong Cham", "Kampong Chhnang", "Kampong Speu", "Kampong Thom", "Kampot", "Kandal", "Kep", "Koh Kong", "Kratié", "Mondulkiri", "Oddar Meanchey", "Pailin", "Phnom Penh", "Preah Sihanouk", "Preah Vihear", "Prey Veng", "Pursat", "Ratanakiri", "Siem Reap", "Stung Treng", "Svay Rieng", "Takéo", "Tboung Khmum",
+]
+
+print(provices[9])
