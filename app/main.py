32ca83b276dd5297a972cfc6d3b86ac850859e5f
lhsato.kh <lhsato.kh@gmail.com>
2026-02-18 21:26:28 +0700

    4. Python Basics for Web Scraping adding population

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-provinces = ["Banteay Meanchey", "Battambang", "Kampong Cham", "Kampong Chhnang", "Kampong Speu", "Kampong Thom", "Kampot", "Kandal", "Kep", "Koh Kong", "Kratié", "Mondulkiri", "Oddar Meanchey", "Pailin", "Phnom Penh", "Preah Sihanouk", "Preah Vihear", "Prey Veng", "Pursat", "Ratanakiri", "Siem Reap", "Stung Treng", "Svay Rieng", "Takéo", "Tboung Khmum",
+import pandas as pd 
+
+provinces = ["Banteay Meanchey", "Battambang", "Kampong Cham", "Kampong Chhnang", "Kampong Speu", "Kampong Thom", "Kampot", "Kandal", "Kep", "Koh Kong", "Kratié", "Mondulkiri", "Oddar Meanchey", "Pailin", "Phnom Penh", "Preah Sihanouk", "Preah Vihear", "Prey Veng", "Pursat", "Ratanakiri", "Siem Reap", "Stung Treng", "Svay Rieng", "Takéo", "Tboung Khmum",]
+
+populations = [898484,1132017,1062914,604895,924175,807254,682987,1352198,48772,140962,441078,93657,267703,79445,2352851,234702,249973,1277867,516072,235852,1099825,176488,613159,1097243,889970,
 ]
 
 # print(provinces[-25])
