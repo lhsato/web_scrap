9d90efea43433148b2232459c58ad731cd898ff7
lhsato.kh <lhsato.kh@gmail.com>
2026-02-23 21:24:12 +0700

    11. How To Get The HTML from a Website

---
+++ app/Section 02/11.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+response = requests.get(website)
+content = response.text
+
+soup = BeautifulSoup(content,'lxml')
+print(soup.prettify())
