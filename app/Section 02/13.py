688e8b59fdac779b79375722d2228cdfa5761156
lhsato.kh <lhsato.kh@gmail.com>
2026-02-23 22:19:37 +0700

    getting h1 from https://www.bible.com/bible/1270

--- app/Section 02/13.py
+++ app/Section 02/13.py
@@ -2,18 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 
-website = 'https://subslikescript.com/movie/Titanic-120338'
+# website = 'https://subslikescript.com/movie/Titanic-120338'
+website = 'https://www.bible.com/bible/1270/GEN.1.%2525E1%25259E%252596%2525E1%25259E%252582%2525E1%25259E%252594'
 response = requests.get(website)
 content = response.text
 
 soup = BeautifulSoup(content,'lxml')
 # print(soup.prettify())
 
-box = soup.find('article', class_ = 'main-article')
+# box = soup.find('article', class_ = 'main-article')
+box = soup.find("div", {"data-testid": "chapter-content"})
 
 # soup.find('h1').get_text()
 title = box.find('h1').get_text()
-transcript  = box.find('div', class_ = 'full-script').get_text(strip=True, separator=' ')
+# transcript  = box.find('div', class_ = 'full-script').get_text(strip=True, separator=' ')
 
-with open(f'{title}.txt','w') as file:
-    file.write(transcript)
+# with open(f'{title}.txt','w') as file:
+#     file.write(transcript)
+
+print(title)
