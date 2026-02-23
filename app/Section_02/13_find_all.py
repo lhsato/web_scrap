72932bf377102065f2eff3600718639ce2d516b5
lhsato.kh <lhsato.kh@gmail.com>
2026-02-24 00:43:57 +0700

    get only the lines with transcript

--- app/Section_02/13_find_all.py
+++ app/Section_02/13_find_all.py
@@ -12,7 +12,7 @@ box = soup.find('article', class_ = 'main-article')
 
 title = box.find('h1').get_text()
 # subtitle-cue
-transcripts  = box.find_all('div', class_ = 'subtitle-cue')
+transcripts  = box.find_all('p', class_ = 'cue-line')
 # .get_text(strip=True, separator='\n')
 
 with open(f'{title}_all.txt','w') as file:
